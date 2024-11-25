012a7d6b65be11b86302d7035df1035c52f61361
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-11-25 14:18:54 -0300

    wip: main class

--- src/main/java/com/rocketseat/redirectUrlShortner/Main.java
+++ src/main/java/com/rocketseat/redirectUrlShortner/Main.java
@@ -1,7 +1,52 @@
 package com.rocketseat.redirectUrlShortner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+    private  final S3Client s3Client = S3Client.builder().build();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String pathParameters = (String) input.get("rawPath");
+        String shortUrlCode = pathParameters.replace("/", "");
+
+        if(shortUrlCode == null || shortUrlCode.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'shortUrlCode' is required");
+        }
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("java-shortener-url-storage-test")
+                .key(shortUrlCode + ".json")
+                .build();
+
+        InputStream s3ObjectStream;
+
+        try {
+            s3ObjectStream = s3Client.getObject(getObjectRequest);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error fetching URL data from S3: " + exception.getMessage());
+        }
+
+        OriginalUrlData originalUrlData;
+
+        try {
+            originalUrlData = objectMapper.readValue(s3ObjectStream, OriginalUrlData.class);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error deserializing URL data: " + exception.getMessage());
+        }
+
+        long currentTimeInSeconds = System.currentTimeMillis() / 1000;
+
+
+        return null;
     }
 }
