4da5feedcc6f256bd5ea65b9e543280d1d51c0e9
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-11-26 00:06:10 -0300

    feat: OriginalUrlData class update

--- src/main/java/com/rocketseat/redirectUrlShortner/OriginalUrlData.java
+++ src/main/java/com/rocketseat/redirectUrlShortner/OriginalUrlData.java
@@ -1,15 +1,30 @@
 package com.rocketseat.redirectUrlShortner;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Setter
-@Getter
 public class OriginalUrlData {
     private String originalUrl;
     private long expirationTime;
+
+    public OriginalUrlData() {
+    }
+
+    public OriginalUrlData(String originalUrl, long expirationTime) {
+        this.originalUrl = originalUrl;
+        this.expirationTime = expirationTime;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(long expirationTime) {
+        this.expirationTime = expirationTime;
+    }
 }
